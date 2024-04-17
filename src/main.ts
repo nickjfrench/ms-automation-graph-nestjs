3defa1c94df4b91992d986a2caf598bf7cbbe21e
nickjfrench <64120654+nickjfrench@users.noreply.github.com>
2024-04-17 14:29:10 -0400

    API versioning prefix

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { VersioningType } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // Set Route Prefix to begin with /api/. E.g. localhost:3000/api
+  app.setGlobalPrefix('api');
+
+  // Enable API Version route prefix. E.g. localhost:3000/api/v1/route
+  app.enableVersioning({
+    type: VersioningType.URI,
+    defaultVersion: '1',
+  });
+
   await app.listen(3000);
 }
 bootstrap();
