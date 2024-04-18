1df4bd7091dc678ea21883d8d68ea4581725d3bb
nickjfrench <64120654+nickjfrench@users.noreply.github.com>
2024-04-18 15:51:44 -0400

    Microsoft User Authentication seems to be successful

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,14 +2,18 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
+import { AuthService } from './auth/auth.service';
+import { AuthController } from './auth/auth.controller';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
     }),
+    AuthModule,
   ],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [AppController, AuthController],
+  providers: [AppService, AuthService],
 })
 export class AppModule {}
---
+++ src/auth/auth.controller.ts
@@ -0,0 +1,23 @@
+import { Controller, Get, Query, Res } from '@nestjs/common';
+import { AuthService } from './auth.service';
+import { Response } from 'express';
+
+@Controller('auth')
+export class AuthController {
+  constructor(private authService: AuthService) {}
+
+  @Get('login')
+  async login(@Res() res: Response): Promise<void> {
+    const loginUrl = await this.authService.signIn();
+    res.redirect(loginUrl);
+  }
+
+  @Get('callback')
+  async callback(
+    @Query('code') code: string,
+    @Res() res: Response,
+  ): Promise<void> {
+    const authResponse = await this.authService.handleRedirect(code);
+    res.json(authResponse);
+  }
+}
