69b2b590af5a0d741a08c602ad768ba9ed7c1089
nickjfrench <64120654+nickjfrench@users.noreply.github.com>
2024-04-18 10:01:27 -0400

    Setting up 1Password CLI support

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { VersioningType } from '@nestjs/common';
+import { validateCli } from '@1password/op-js';
+
+// Set development configs here.
+if (process.env.NODE_ENV === 'development') {
+  console.log('Server starting for Development.');
+}
+
+// Validate 1Password when using start:dev_op.
+if (process.env.ONEPASS === 'true') {
+  console.log('1Password CLI support enabled.');
+  validateCli().catch((error) => {
+    throw new Error('1Password CLI is not valid:' + error.message);
+  });
+}
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
