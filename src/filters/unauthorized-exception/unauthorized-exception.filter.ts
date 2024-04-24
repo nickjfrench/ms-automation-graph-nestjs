943277e6029b1adef79953c7ade374d8c7fab11e
nickjfrench <64120654+nickjfrench@users.noreply.github.com>
2024-04-24 10:23:26 -0400

    Cleaning up Unauthorized Exception

--- src/filters/unauthorized-exception/unauthorized-exception.filter.ts
+++ src/filters/unauthorized-exception/unauthorized-exception.filter.ts
@@ -14,20 +14,20 @@ export class UnauthorizedExceptionFilter implements ExceptionFilter {
     const response = ctx.getResponse<Response>();
     const request = ctx.getRequest<Request>();
 
-    // TODO: This needs to be moved to it's own function.
     // Validate and sanitize the afterLoginRedirect URL
     const afterLoginRedirect = request.url;
-    try {
-      const url = new URL(afterLoginRedirect, `http://${request.headers.host}`);
-      if (url.hostname === request.hostname) {
-        request.session.afterLoginRedirect = afterLoginRedirect;
-      } else {
-        throw new BadRequestException('Invalid redirect URL');
-      }
-    } catch (err) {
-      request.session.afterLoginRedirect = '/';
-      throw new BadRequestException('Invalid redirect URL');
+
+    // Validate the redirect URL before storing it in the session.
+    if (!this.validateRedirectHostname(afterLoginRedirect, request)) {
+      console.error(
+        '400 Bad Request: Invalid request hostname: ' + request.hostname,
+      );
+      throw new BadRequestException(
+        'Invalid request hostname: ' + request.hostname,
+      );
     }
+
+    request.session.afterLoginRedirect = afterLoginRedirect;
 
     // Log the error message.
     console.error(
@@ -36,5 +36,19 @@ export class UnauthorizedExceptionFilter implements ExceptionFilter {
 
     // Redirect to get authorization.
     response.redirect('auth/login');
+  }
+
+  private validateRedirectHostname(
+    afterLoginRedirectUrl: string,
+    req: Request,
+  ): boolean {
+    // Ensure redirect URL is on the same site as the request.
+    const url = new URL(afterLoginRedirectUrl, `http://${req.headers.host}`);
+
+    if (url.hostname !== req.hostname) {
+      return false;
+    }
+
+    return true;
   }
 }
